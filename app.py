f815c584b97b0ea10f31e674d512f92cfd0e7fa0
Himanshu Mishra <himanshu2014iit@gmail.com>
2018-01-19 14:11:37 +0530

    Fix session error

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import request
-from flask import Flask, render_template
-from flask_session import Session
+import requests
+from flask import Flask, render_template, session
+# from flask_session import Session
 
 
 APP_SECRET_KEY = 'blockchain'
@@ -13,15 +13,13 @@ app.secret_key = APP_SECRET_KEY
 app.config['SESSION_TYPE'] = APP_CONFIG_SESSION_TYPE
 app.debug = APP_DEBUG
 
-sess = Session()
-
 # https://pythonspot.com/login-authentication-with-flask/
 
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        sess['logged_in'] = True
+        session['logged_in'] = True
     else:
         print('wrong password!')
     return main()
@@ -29,19 +27,19 @@ def do_admin_login():
 
 @app.route("/logout")
 def logout():
-    sess['logged_in'] = False
+    session['logged_in'] = False
     return main()
 
 
 @app.route("/")
 def main():
-    if sess['logged_in']:
+    if session.get('logged_in', False):
         return render_template('home.html')
     else:
         return render_template('main.html')
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8081)
 
 '''
  - Blockchain event
